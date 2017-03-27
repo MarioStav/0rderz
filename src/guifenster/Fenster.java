ed1044eff591bf588a4d4b5cc18d991b9ffec974
Mario <marioxp83@hotmail.com>
2017-03-27 11:24:41 +0200

    Neus Fensta

---
+++ src/guifenster/Fenster.java
@@ -0,0 +1,10 @@
+package guifenster;
+
+/**
+ * Created by Mario on 27.03.2017.
+ */
+public class Fenster {
+    public static void main(String[] args){
+        System.out.println("sexy");
+    }
+}
